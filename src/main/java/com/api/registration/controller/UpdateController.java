a6ea9d4941bb02457630160d555e7e6dd537b674
dev-umair02 <um.ahsan02@gmail.com>
2025-02-11 13:06:40 +0530

    Fixed the defect-123-submit button was disabled

---
+++ src/main/java/com/api/registration/controller/UpdateController.java
@@ -0,0 +1,7 @@
+package com.api.registration.controller;
+
+public class UpdateController {
+    public static void main(String[] args) {
+        System.out.println("This is update controller");
+    }
+}
